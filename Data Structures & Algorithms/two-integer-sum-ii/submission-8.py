bd61666f4a3da6d1d66469227b760c37d79b7d1d
Jayanth <133797637+XElJayX@users.noreply.github.com>
2026-09-10 13:04:13 -0500

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            key = numbers[i]
+            find = target - key
+            start = i+1
+            end = len(numbers) - 1
+            while start <= end:
+                mid = (start+end)//2
+                if numbers[mid] == find:
+                    return [i+1, mid+1]
+                    break
+                else:
+                    if find > numbers[mid]:
+                        start = mid + 1
+                    else:
+                        end = mid - 1
+
